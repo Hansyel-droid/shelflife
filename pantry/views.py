408e34e60cd6dceaff827cc31f9882df4f36daff
unknown <rjlopezvelete@gmail.com>
2026-05-23 00:14:35 +0800

    feat: models, serializers, CRUD viewsets, admin

--- pantry/views.py
+++ pantry/views.py
@@ -6,6 +6,16 @@ class PantryItemViewSet(viewsets.ModelViewSet):
     queryset = PantryItem.objects.all()
     serializer_class = PantryItemSerializer
 
+    def get_queryset(self):
+        qs = super().get_queryset()
+        category = self.request.query_params.get('category')
+        search = self.request.query_params.get('search')
+        if category:
+            qs = qs.filter(category=category)
+        if search:
+            qs = qs.filter(name__icontains=search)
+        return qs
+
 class ShoppingListViewSet(viewsets.ModelViewSet):
     queryset = ShoppingList.objects.all()
     serializer_class = ShoppingListSerializer
